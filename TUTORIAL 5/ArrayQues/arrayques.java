542b271d6549733a2bfe481c38ab1535bbc1aacd
ADITYA_MISHRA <90920322+Adityamishra45@users.noreply.github.com>
2023-04-13 23:55:31 +0530

    Add files via upload

---
+++ TUTORIAL 5/ArrayQues/arrayques.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class arrayques
+{
+    public static void main(String[] args) {
+        int[] arr = { 1, 3, 5, 5, 5, 5, 67, 123, 125 };
+
+        Scanner sc = new Scanner (System.in);
+        int target = sc.nextInt();
+        System.out.println(wealth(arr,target));
+
+    }
+    static int wealth(int[]arr, int target)
+    {
+        ArrayList ar = new ArrayList();
+        // int ans =0; 
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]==target)
+            {
+                ar.add(i);
+               
+            }
+        }
+        
+        System.out.println(ar.get(0));
+            int last = (int) ar.get(ar.size() - 1);
+            // System.out.println(last);
+        return last;
+        
+    }
+}
