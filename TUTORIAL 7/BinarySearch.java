c4f37737880e2a625c15b7ed4ad7b62cdc05f125
ADITYA_MISHRA <90920322+Adityamishra45@users.noreply.github.com>
2023-05-11 23:38:45 +0530

    Add files via upload

---
+++ TUTORIAL 7/BinarySearch.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+public class BinarySearch
+{
+    public static void main(String[] args) {
+        int[] arr = {-5,8,9,15,56,78,79,99,100};
+        int target = 78;
+        int ans = binarysearch(arr, target);
+        System.out.println(ans);
+    }
+    static int binarysearch(int[]arr ,  int target)
+    {
+        // First we need to find the middle element;
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start<=end) //Because we have to check the middle element again and again.
+        {
+            int mid = start+(end-start)/2;
+            if(target<arr[mid])
+            {
+                end = mid-1;
+            }
+            else if(target>arr[mid])
+            {
+                start = mid+1;
+            }
+            else{
+                return mid;
+            }
+           
+        }
+        return -1;
+        
+    }
+    
+}
