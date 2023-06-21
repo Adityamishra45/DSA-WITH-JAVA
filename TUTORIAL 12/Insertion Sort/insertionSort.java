ff5e0e09da33781b06ccefec9e302bb7c68ec568
ADITYA_MISHRA <90920322+Adityamishra45@users.noreply.github.com>
2023-06-21 18:21:44 +0530

    Add files via upload

---
+++ TUTORIAL 12/Insertion Sort/insertionSort.java
@@ -0,0 +1,31 @@
+//Increasing order sorting
+
+import java.util.*;
+public class insertionSort{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void insertionSort(int[] arr){
+        for (int i = 0; i <= arr.length-2; i++) //we are checking upto length -2 because at first step when i is 0 it is comparing with the 0th and 1st index.
+        {
+            for (int j = i+1; j > 0; j--) {
+                if(arr[j]<arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+                else{
+                    break;
+                }
+            }
+            
+        }
+    }
+}
